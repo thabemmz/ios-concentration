d7521e165c20cbe52d3484625c749a5b8f9b4cf0
thabemmz <github@cvbemmel.nl>
2019-08-01 14:34:26 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Christiaan van Bemmel on 01/08/2019.
+//  Copyright © 2019 Christiaan van Bemmel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
